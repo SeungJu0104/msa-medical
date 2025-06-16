7557bbe8149461941d5863230cf7074876110065
DESKTOP-684DG1A\SeungJu <skatmdwn23@naver.com>
2025-06-16 16:05:05 +0900

    환자 직접 예약 등록 Vue 생성 및 의사 선택 드롭다운 작성.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import regReservationByPatient from '@/views/regReservationByPatient.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/regReservationByPatient',
+      name: 'regReservationByPatient',
+      component: regReservationByPatient,
     },
     {
       path: '/about',
---
+++ frontend/src/views/regReservationByPatient.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+  import {ref, reactive} from 'vue'
+
+  const doctorList = ref(
+[
+      {uuid: "1", name: "김수영"},
+      {uuid: "2", name: "박민수"}
+    ]
+  );
+  const selectedDoctor = ref({
+    uuid: '',
+    name: ''
+  });
+
+  const selectDoctor = (doctor) => {
+    selectedDoctor.value.uuid = doctor.uuid;
+    selectedDoctor.value.name = doctor.name;
+  }
+
+
+</script>
+
+<template>
+  <div class="container">
+    <h2>예약등록</h2>
+    <div class="dropdown">
+      <h3>의사 : </h3>
+      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        {{ selectedDoctor?.name || '의사를 선택해주세요.' }}
+      </button>
+      <ul class="dropdown-menu">
+          <template v-for="doctor in doctorList" :key="doctor.uuid">
+            <li class="dropdown-item" @click="selectDoctor(doctor)">{{doctor.name}}</li>
+          </template>
+      </ul>
+    </div>
+    <div class="">
+      <h3>일자</h3>
+      
+
+    </div>
+  </div>
+</template>
