a12c2db116707bd292ee4f71a7e00d40398248e6
Conut-1 <1mim1@naver.com>
2025-06-16 17:55:05 +0900

    feat: reason 없이 ResponseStatusException을 throw 했을 때 status code의 기존 메세지를 출력한다.
    Optional을 사용해 reason이 null인 경우를 판별한다.

--- server/src/main/java/com/emr/slgi/exception/GlobalExceptionHandler.java
+++ server/src/main/java/com/emr/slgi/exception/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package com.emr.slgi.exception;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,7 +15,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<Map<String, Object>> handleResponseStatusException(ResponseStatusException exception) {
         Map<String, Object> map = new HashMap<>();
-        map.put("message", exception.getReason());
+        map.put(
+            "message",
+            Optional.ofNullable(exception.getReason()).orElse(((HttpStatus)exception.getStatusCode()).getReasonPhrase())
+        );
         return ResponseEntity.status(exception.getStatusCode()).body(map);
     }
 }
