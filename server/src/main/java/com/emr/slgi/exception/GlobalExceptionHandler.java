9c050cc2e3436611711c81a01d401d7f36eeef8b
Conut-1 <1mim1@naver.com>
2025-06-16 16:53:22 +0900

    feat: 전역 예외 처리기 등록
    ResponseStatusException에 대해서 해당 응답에 대한 status code를 설정하고, { message: "이유" } 형식으로 응답한다.

---
+++ server/src/main/java/com/emr/slgi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.emr.slgi.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, Object>> handleResponseStatusException(ResponseStatusException exception) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("message", exception.getReason());
+        return ResponseEntity.status(exception.getStatusCode()).body(map);
+    }
+}
