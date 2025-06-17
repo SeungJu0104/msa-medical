036d7818357ee6f5f1188ef54e8db10f64805ffa
Conut-1 <1mim1@naver.com>
2025-06-17 14:04:13 +0900

    feat: Member 클래스 작성
    Member에 대한 속성을 String과 LocalDateTime만으로 작성

---
+++ server/src/main/java/com/emr/slgi/member/Member.java
@@ -0,0 +1,18 @@
+package com.emr.slgi.member;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    // TODO: role을 DB에서 가져와서 처리 가능하도록 수정
+    private String uuid;
+    private String role;
+    private String name;
+    private String rrn;
+    private String phone;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+    private LocalDateTime deleteDate;
+}
