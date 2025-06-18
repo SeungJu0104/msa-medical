561a4bbb84a8ad7b053d669fdfae0fd528762699
DESKTOP-684DG1A\SeungJu <skatmdwn23@naver.com>
2025-06-18 14:04:16 +0900

    [커밋메세지]
    [프론트엔드]
    1. jsconfig.json 파일에 ES2017 버전 라이브러리 추가.
    2. endpoint.js에 환자 직접 예약 관련 method와 url을 reservation 속성으로 추가.
    3. endpoint.js에 의사 목록 관련 method와 url을 list 속성으로 추가.
    4. regReservationByPatient.vue에 1-3번 관련 내용들 수정 및 9시부터 6시까지 10분 단위로 분할해 버튼으로 표시.
    
    [서버]
    1. DoctorController에 의사 목록을 가져오는 getDoctorList 함수 수정.
    2. DoctorController에 예약 시 데이터 검증 수행할 ReservationForm 작성.
    
    []

--- frontend/src/views/regReservationByPatient.vue
+++ frontend/src/views/regReservationByPatient.vue
@@ -4,41 +4,79 @@ import VueDatepicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
 import { customFetch } from '@/util/customFetch'
 import router from '@/router'
+import { ENDPOINTS } from '@/util/endpoints'
 
-  const doctorList = ref();
-  const selectedDoctor = ref({
-    uuid: '',
-    name: ''
+  const selectedVal = reactive({
+    doctorUuid: null,
+    date: new Date(),
+    time: null,
+    symptom: null
   });
 
+  const doctorList = ref();
+
   const selectDoctor = (doctor) => {
-    console.log(doctor.uuid);
-    console.log(doctor.name);
-    selectedDoctor.value.uuid = doctor.uuid;
-    selectedDoctor.value.name = doctor.name;
+    selectedVal.doctorUuid = doctor.uuid;
+    console.log(selectedVal);
   }
-const minDate = new Date();
-let maxDate = new Date();
-maxDate.setDate(minDate.getDate() + 7);
-// const allowedDates = computed(() => {
-  //   const dates = [];
-  //   let date;
-  //   for(let i = 0; i < 7; i++){
-  //     date = new Date(new Date().setDate(new Date().getDate() + i));
-  //     dates.push(date);
-  //   }
-  //   return dates;
-  // });
 
-  let selectedTime = ref([]);
+  const minDate = new Date();
+  const maxDate = new Date().setDate(minDate.getDate() + 7);
 
-  let selectedDate = ref(new Date());
+  const reservationTime = computed(() => {
+
+    const start = new Date();
+    const end = new Date();
+    const slots = [];
+
+    start.setHours(9, 0, 0, 0); // 9:00 AM
+    end.setHours(18, 0, 0, 0); // 6:00 PM
+
+    while (start <= end) {
+
+      const hours = start.getHours().toString().padStart(2, '0');
+      const minutes = start.getMinutes().toString().padStart(2, '0');
+
+      slots.push(`${hours}:${minutes}`);
+      start.setMinutes(start.getMinutes() + 10);
+
+    }
+
+    return slots;
+
+  });
+
+  const selectTime = (time) => {
+    selectedVal.time = time;
+    console.log(selectedVal);
+  }
+
+  const historyBack = () => {
+    router.push({name: 'home'});
+  }
+
+  const reservation = () => {
+    customFetch(
+      ENDPOINTS.member.reservation
+    )
+    .then(
+        response => {
+          if(response.status === 200) alert(response.data?.message);
+        }
+    )
+    .catch(
+        err => {
+          err.response?.data?.message ? alert(err.response?.data?.message) : alert("실행 중 오류가 발생했습니다. 다시 실행해주세요.");
+        }
+    )
+  }
 
   onMounted(() =>
-      customFetch({
-        method: 'get',
-        url: '/doctor/list'
-      })
+      customFetch(
+        // method: 'get',
+        // url: '/doctor/list'
+        ENDPOINTS.doctor.list
+      )
       .then(
           response => {
             if (response.status === 200) {
@@ -63,7 +101,7 @@ maxDate.setDate(minDate.getDate() + 7);
     <div class="dropdown my-3">
       <h3>의사</h3>
       <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        {{ selectedDoctor?.name || '의사를 선택해주세요.' }}
+        {{ selectedVal?.name || '의사를 선택해주세요.' }}
       </button>
       <ul class="dropdown-menu">
           <template v-for="doctor in doctorList" :key="doctor.uuid">
@@ -74,13 +112,25 @@ maxDate.setDate(minDate.getDate() + 7);
     <div class="my-3">
       <h3>일자</h3>
       <VueDatepicker
-          v-model="selectedDate" :format="'yyyy-MM-dd'" :min-date="minDate" :max-date="maxDate"
+          v-model="selectedVal.date" :format="'yyyy-MM-dd'" :min-date="minDate" :max-date="maxDate"
           :enable-time-picker="false"  :input-class="'form-control'" prevent-min-max-navigation
       />
     </div>
     <div class="my-3">
       <h3>시간</h3>
-
+      <template v-for="time in reservationTime">
+        <button type="button" class="btn btn-primary btn-lg" @click="selectTime(time)">{{time}}</button>
+      </template>
+    </div>
+    <div>
+      <div class="my-3 input-group">
+        <span class="input-group-text">증상</span>
+        <textarea class="form-control" aria-label="symptom" v-model="selectedVal.symptom"></textarea>
+      </div>
+    </div>
+    <div>
+      <button type="button" class="btn btn-outline-success" @click="reservation">예약</button>
+      <button type="button" class="btn btn-outline-warning" @click="historyBack">취소</button>
     </div>
   </div>
 </template>
