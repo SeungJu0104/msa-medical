42a0138e83d10eb6461a993528cc62173297e120
Conut-1 <1mim1@naver.com>
2025-06-18 11:10:57 +0900

    feat: 메인페이지 작성

---
+++ frontend/src/components/home/layout/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>------------</div>
+    <div>로고</div>
+    <div>
+      <div>진료예약</div>
+      <div>병원소개</div>
+      <div>오시는길</div>
+    </div>
+    <div>
+      <div>
+        <RouterLink>로그인</RouterLink>
+        <RouterLink>회원가입</RouterLink>
+      </div>
+      <div>
+        <RouterLink>회원님</RouterLink>
+        <RouterLink>로그아웃</RouterLink>
+      </div>
+    </div>
+    <div>------------</div>
+  </div>
+</template>
+
+<script setup>
+</script>
