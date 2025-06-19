956db8c090b1b47b012eafe25a70d42c73dd6297
Conut-1 <1mim1@naver.com>
2025-06-19 10:16:23 +0900

    fix: router에서 home을 MainView로 설정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -11,12 +11,11 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
       component: HomeView,
       children: [
         {
           path: '/',
-          name: 'mainView',
+          name: 'home',
           component: MainView
         },
         ...patientRoutes
