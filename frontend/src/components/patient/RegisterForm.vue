d55ce75918332e5625f5e542326f9b66b8aa39b7
Conut-1 <1mim1@naver.com>
2025-06-19 10:15:44 +0900

    feat: 환자 회원가입 입력 form 작성
    각 항목에 대한 input 추가.
    취소 버튼을 누를 시에 메인페이지로 되돌아감

---
+++ frontend/src/components/patient/RegisterForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <form>
+    <div>
+      <label for="userid">아이디</label>
+      <input type="text" id="userid" v-model="member.userid">
+    </div>
+    <div>
+      <label for="password">비밀번호</label>
+      <input type="password" id="password" v-model="member.password">
+    </div>
+    <div>
+      <label for="name">이름</label>
+      <input type="text" id="name" v-model="member.name">
+    </div>
+    <div>
+      <label for="rrn">주민번호</label>
+      <input type="text" id="rrn" v-model="member.rrn">
+    </div>
+    <div>
+      <label for="phone">전화번호</label>
+      <input type="text" id="phone" v-model="member.phone">
+    </div>
+  </form>
+  <button @click="registerPatient">회원가입</button>
+  <RouterLink :to="{ name: 'home' }">취소</RouterLink>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+const member = reactive({
+  userid: '',
+  password: '',
+  name: '',
+  phone: '',
+  rrn: ''
+});
+
+async function registerPatient() {
+  console.log("register patient");
+}
+</script>
