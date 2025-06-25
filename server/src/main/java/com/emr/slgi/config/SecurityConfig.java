ef78208a460aba2499e8ac8e433d37242d7a0daf
Conut-1 <1mim1@naver.com>
2025-06-25 16:11:26 +0900

    feat: 메서드 시큐리티 활성화
    @PreAuthorize 사용을 위한 메서드 시큐리티 활성화

--- server/src/main/java/com/emr/slgi/config/SecurityConfig.java
+++ server/src/main/java/com/emr/slgi/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.emr.slgi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
