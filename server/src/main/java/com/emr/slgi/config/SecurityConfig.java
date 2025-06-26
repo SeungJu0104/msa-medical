8e3520311fb273a3ac5a509647c73bfd7d25f6ce
Conut-1 <1mim1@naver.com>
2025-06-26 23:26:40 +0900

    fix: 예외가 발생해서 /error로 포워딩되었을 때 시큐리티 필터 체인에서 잡는 문제 해결

--- server/src/main/java/com/emr/slgi/config/SecurityConfig.java
+++ server/src/main/java/com/emr/slgi/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
         return http
             .csrf((csrf) -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**").permitAll()
+                .requestMatchers("/auth/**", "/error").permitAll()
                 .anyRequest().authenticated()
             )
             .exceptionHandling(exceptionHandling -> exceptionHandling
