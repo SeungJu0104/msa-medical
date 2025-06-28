d95efbf0f6a4b2a4ad401d593f38330c023e56ae
suyungking <105546111+suyungking@users.noreply.github.com>
2025-06-28 22:02:57 +0900

    feat : 첨부파일 등록및 보기 기능

--- server/src/main/java/com/emr/slgi/attachment/AttachmentDAO.java
+++ server/src/main/java/com/emr/slgi/attachment/AttachmentDAO.java
@@ -1,5 +1,13 @@
 package com.emr.slgi.attachment;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface AttachmentDAO {
 
+	void insertAttachment(Attachment att);
+
+	List<Attachment> selectAttachments(int id);
 }
