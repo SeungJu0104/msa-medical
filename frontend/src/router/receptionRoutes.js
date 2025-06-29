3132bf05f7c83328491416a13c61f3a639570c2c
Conut-1 <1mim1@naver.com>
2025-06-29 14:09:20 +0900

    refactor: 사용하지 않는 import 제거

--- frontend/src/router/receptionRoutes.js
+++ frontend/src/router/receptionRoutes.js
@@ -1,7 +1,4 @@
 const AcceptPatientByStaff = () => import("@/views/reception/AcceptPatientByStaff.vue");
-const WaitingListPatientList = () => import('@/views/reception/WaitingListPatientList.vue');
-const WaitingListDoctorName = () => import("@/views/reception/WaitingListDoctorName.vue");
-const WaitingListPerDoctor = () => import('@/views/reception/WaitingListPerDoctor.vue');
 
 export const receptionRoutes = [
     {
