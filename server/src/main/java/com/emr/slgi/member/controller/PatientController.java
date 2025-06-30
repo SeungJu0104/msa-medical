1a940cb22a2db298f342c1002d31f0f4198d5638
Conut-1 <1mim1@naver.com>
2025-06-30 15:56:16 +0900

    feat: PatientController 인가 추가

--- server/src/main/java/com/emr/slgi/member/controller/PatientController.java
+++ server/src/main/java/com/emr/slgi/member/controller/PatientController.java
@@ -2,6 +2,7 @@ package com.emr.slgi.member.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,12 +22,14 @@ public class PatientController {
 
     private final MemberService memberService;
 
+    @PreAuthorize("hasAnyRole('DOCTOR', 'NURSE')")
     @PostMapping
     public ResponseEntity<?> registerByStaff(@RequestBody PatientRegisterDTO patientRegisterDTO) {
         memberService.createPatient(patientRegisterDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @PreAuthorize("hasAnyRole('DOCTOR', 'NURSE')")
     @GetMapping("search")
     public ResponseEntity<?> search(PatientSearchDTO patientSearchDTO) {
         return ResponseEntity.ok(memberService.search(patientSearchDTO));
