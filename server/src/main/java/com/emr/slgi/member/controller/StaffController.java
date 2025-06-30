115bdb45b449eb63c4284249f1cf4bee6466af7e
Conut-1 <1mim1@naver.com>
2025-06-30 16:43:28 +0900

    feat: 의료진 목록 가져오기 인가 추가

--- server/src/main/java/com/emr/slgi/member/controller/StaffController.java
+++ server/src/main/java/com/emr/slgi/member/controller/StaffController.java
@@ -3,6 +3,7 @@ package com.emr.slgi.member.controller;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 public class StaffController {
   private final MemberService memberService;
 
+  @PreAuthorize("hasAnyRole('DOCTOR', 'NURSE')")
   @GetMapping("/list")
   public ResponseEntity<?> getStaffList(@AuthenticationPrincipal String uuid) {
     return ResponseEntity.ok(Map.of("list", memberService.getStaffList(uuid)));
