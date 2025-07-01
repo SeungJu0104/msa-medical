3d82e01c3bd093c9632a859a7645c649a62c7be8
Conut-1 <1mim1@naver.com>
2025-07-01 16:36:24 +0900

    feat: 유효성 검사 추가

--- server/src/main/java/com/emr/slgi/auth/dto/LogoutDTO.java
+++ server/src/main/java/com/emr/slgi/auth/dto/LogoutDTO.java
@@ -1,8 +1,13 @@
 package com.emr.slgi.auth.dto;
 
+import com.emr.slgi.common.constants.RegexPatterns;
+
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class LogoutDTO {
+  @NotBlank
+  @Pattern(regexp = RegexPatterns.JWT_TOKEN)
   private String refreshToken;
 }
