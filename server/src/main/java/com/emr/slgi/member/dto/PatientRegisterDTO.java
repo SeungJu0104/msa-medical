7e4539a143321a02beb47d4ce272c47890b7036b
Conut-1 <1mim1@naver.com>
2025-07-03 09:41:04 +0900

    feat: 패턴 유효성 검사 추가

--- server/src/main/java/com/emr/slgi/member/dto/PatientRegisterDTO.java
+++ server/src/main/java/com/emr/slgi/member/dto/PatientRegisterDTO.java
@@ -1,18 +1,21 @@
 package com.emr.slgi.member.dto;
 
+import com.emr.slgi.common.constants.RegexPatterns;
+
 import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class PatientRegisterDTO {
-    // TODO: 패턴 추가 필요
     // TODO: record로 변경 테스트
     @NotBlank
     private final String name;
 
     @NotBlank
+    @Pattern(regexp = RegexPatterns.RRN)
     private final String rrn;
 
     @NotBlank
+    @Pattern(regexp = RegexPatterns.PHONE)
     private final String phone;
 }
