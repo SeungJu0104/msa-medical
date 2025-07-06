8d79a6301f4b028f72171bc2f80c919404fe72a2
DESKTOP-684DG1A\SeungJu <skatmdwn23@naver.com>
2025-07-06 16:14:21 +0900

    feat: 환자 이름으로 환자 정보 가져오기.

---
+++ frontend/src/router/staffRoutes.js
@@ -0,0 +1,11 @@
+const Search = () => import ('@/views/staff/Search.vue');
+
+export const staffRoutes = [
+
+    {
+        path: 'search',
+        name: 'search',
+        component: Search
+    }
+
+]
