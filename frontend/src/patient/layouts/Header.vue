e08505e047e78861ae577fa0d1e1ef8a426225c9
Conut-1 <1mim1@naver.com>
2025-07-07 13:54:52 +0900

    design: 헤더 CSS 추가

--- frontend/src/patient/layouts/Header.vue
+++ frontend/src/patient/layouts/Header.vue
@@ -1,24 +1,41 @@
 <template>
-  <div>
-    <div>------------</div>
-    <RouterLink :to="{ name: 'home' }">로고</RouterLink>
-    <div>
-      <div>진료예약</div>
-      <div>병원소개</div>
-      <div>오시는길</div>
-    </div>
-    <div>
-      <div>
-        <RouterLink :to="{ name: 'loginView' }">로그인</RouterLink>
-        <RouterLink :to="{ name: 'patientRegister' }">회원가입</RouterLink>
+  <header>
+    <nav class="navbar navbar-expand navbar-light bg-secondary-subtle py-0">
+      <div class="container-fluid justify-content-end">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink :to="{ name: 'loginView' }" class="nav-link px-2 text-secondary fs-sm">로그인</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink :to="{ name: 'patientRegister' }" class="nav-link px-2 text-secondary fs-sm">회원가입</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink :to="{ name: 'mypage' }" class="nav-link px-2 text-secondary fs-sm">회원님</RouterLink>
+          </li>
+          <li class="nav-item">
+            <LogoutButton class="nav-link px-2 text-secondary fs-sm" />
+          </li>
+        </ul>
       </div>
-      <div>
-        <RouterLink :to="{ name: 'mypage' }">회원님</RouterLink>
-        <LogoutButton />
+    </nav>
+  
+    <nav class="navbar navbar-expand navbar-light bg-white border-bottom py-3">
+      <div class="container-fluid">
+        <RouterLink :to="{ name: 'home' }" class="navbar-brand me-4">로고</RouterLink>
+        <ul class="navbar-nav mx-auto mb-0 overflow-auto">
+          <li class="nav-item px-2">
+            <RouterLink class="nav-link fw-bold fs-lg">진료 예약</RouterLink>
+          </li>
+          <li class="nav-item px-2">
+            <RouterLink class="nav-link fw-bold fs-lg">병원 소개</RouterLink>
+          </li>
+          <li class="nav-item px-2">
+            <RouterLink class="nav-link fw-bold fs-lg">오시는 길</RouterLink>
+          </li>
+        </ul>
       </div>
-    </div>
-    <div>------------</div>
-  </div>
+    </nav>
+  </header>
 </template>
 
 <script setup>
