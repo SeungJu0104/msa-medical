d2c0dd2878c81f4c916ad2b8f55c18a44d0beed6
DESKTOP-684DG1A\SeungJu <skatmdwn23@naver.com>
2025-07-07 13:26:57 +0900

    remove: sql 폴더 삭제.

--- sql/TB_DISEASE.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `TB_DISEASE` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `code` varchar(10) NOT NULL,
-  `create_date` datetime NOT NULL DEFAULT current_timestamp(),
-  `update_date` datetime DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=65536 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- sql/TB_MEDICINE.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `TB_MEDICINE` (
-  `code` varchar(10) NOT NULL,
-  `name` varchar(300) NOT NULL,
-  `create_date` datetime NOT NULL DEFAULT current_timestamp(),
-  `update_date` datetime DEFAULT NULL,
-  `company_name` varchar(100) NOT NULL,
-  PRIMARY KEY (`code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
