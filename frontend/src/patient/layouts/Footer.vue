f1d136fab4e092f5a958f288d3605a8862ea0a9c
Conut-1 <1mim1@naver.com>
2025-07-07 14:27:03 +0900

    design: 푸터 CSS 추가

--- frontend/src/patient/layouts/Footer.vue
+++ frontend/src/patient/layouts/Footer.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <div>-----------</div>
-    <div>병원 전화번호</div>
-    <div>병원 위치</div>
-    <div>카피라이트</div>
-    <div>-----------</div>
-  </div>
+  <footer class="bg-dark text-light py-3 fs-sm">
+    <div class="container text-center">
+      <div>병원전화</div>
+      <div>병원위치</div>
+      <div>&copy; 2025 Team MSA. ALL RIGHTS RESERVED.</div>
+    </div>
+  </footer>
 </template>
 
 <script setup>
