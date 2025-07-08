27eefc14375a104150db7eab1d182247eb9959f5
Conut-1 <1mim1@naver.com>
2025-07-08 14:56:22 +0900

    design: 로그인 화면 CSS

--- frontend/src/patient/views/LoginView.vue
+++ frontend/src/patient/views/LoginView.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>로그인</div>
-  <LoginForm />
+  <div class="d-flex flex-column justify-content-center align-items-center mb-5">
+    <h2 class="fw-bold mt-5">로그인</h2>
+    <hr class="w-100" style="max-width: 720px;" />
+    <LoginForm />
+  </div>
 </template>
 
 <script setup>
