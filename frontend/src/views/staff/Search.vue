bb5157c2ab43663012bdbda7ae63d3e7c9220a7f
DESKTOP-684DG1A\SeungJu <skatmdwn23@naver.com>
2025-07-09 16:52:57 +0900

    feat: 검색 결과 Vue 생성 및 예약 버튼 누를 시 검색 결과 넘겨주도록 구현.

--- frontend/src/views/staff/Search.vue
+++ frontend/src/views/staff/Search.vue
@@ -4,13 +4,17 @@ import {reactive, ref} from "vue";
   import {customFetch} from "@/util/customFetch.js";
   import {ENDPOINTS} from "@/util/endpoints.js";
   import {common} from "@/util/common.js";
+import {patientMethods} from "@/reservation/util/reservation.js";
+import RegReservationByPatient from "@/reservation/views/RegReservationByPatient.vue";
+import {useRouter} from 'vue-router'
 
   const searchVal = reactive({
     input: ''
   });
 
+  const router = useRouter();
   const searchRes = ref();
-  const emit = defineEmits(['searchRes']);
+  // const emit = defineEmits(['searchRes']);
 
   const search = async () => {
 
@@ -23,7 +27,7 @@ import {reactive, ref} from "vue";
         searchRes.value = response.data?.list;
         console.log(searchRes.value);
 
-        emit('searchRes', searchRes.value);
+        // emit('searchRes', searchRes.value);
 
       }
 
@@ -35,6 +39,21 @@ import {reactive, ref} from "vue";
 
   }
 
+  const reservation = (uuid) => {
+
+    router.push({
+      name: 'regReservationByPatient',
+      query: {patientUuid: uuid}
+    });
+
+  }
+
+  const waiting = () => {
+
+
+
+  }
+
 </script>
 
 <template>
@@ -43,8 +62,24 @@ import {reactive, ref} from "vue";
     <div class="input-group mb-3">
       <input class="form-control form-control-lg" type="text" v-model="searchVal.input" minlength="1" maxlength="6" placeholder="환자명"/>
       <button class="btn btn-outline-secondary" type="button" @click="search">
-        <img src="@/assets/search.png" alt="검색" style="width: 20px; height: 20px;" />
+        <img class="search" src="@/assets/search.png" alt="검색" style="width: 20px; height: 20px;" /><!--css 분리하기-->
       </button>
+    </div>
+    <div class="my-3">
+      <table v-for="res in searchRes" :key="res.uuid">
+        <tr>
+          <th>이름</th>
+          <th>생년월일</th>
+          <th>전화번호</th>
+        </tr>
+        <tr>
+          <td v-cloak>{{res.name}}</td>
+          <td v-cloak>{{res.rrn}}</td>
+          <td v-cloak>{{res.phone}}</td>
+          <button class="btn btn-outline-secondary" type="button" @click="reservation(res.uuid)">예약</button>
+          <button class="btn btn-outline-secondary" type="button" @click="waiting">대기</button>
+        </tr>
+      </table>
     </div>
   </div>
 
