ecb530b2e5ef6eee0ef7182f0b308793756a4627
Conut-1 <1mim1@naver.com>
2025-07-11 16:17:06 +0900

    refactor: 라우터 index 정리

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ChatRooms from '@/components/chat/ChatRooms.vue'
-import ChatRoom from '@/components/chat/ChatRoom.vue'
 import { patientRoutes } from './patientRoutes';
 import { adminRoutes } from './adminRoutes';
-import MedicalTreatment from '@/components/diagnosis/MedicalTreatment.vue';
-import {staffRoutes} from "@/router/staffRoutes.js";
-import PaymentList from '@/components/payment/PaymentList.vue';
-
-const OtherView = () => import('@/views/other/OtherView.vue');
-const Reception = () => import('@/reception/views/WaitingList.vue')
-const StaffLayOut = () => import('@/views/staff/StaffMainView.vue');
+import { staffRoutes } from './staffRoutes';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,27 +9,11 @@ const router = createRouter({
     ...patientRoutes,
     ...adminRoutes,
     {
-      path: '/other',
-      name: 'other',
-      component: OtherView
-    },
-    
-    
-    {
-      path: '/medicalTreatment',
-      name : 'medicalTreatment',
-      component:MedicalTreatment
-    },
-    {
       path: '/staff',
       children: [
         ...staffRoutes
       ]
     },
-    {
-      path:`/paymentList`,
-      component:PaymentList
-    }
   ],
 })
 
