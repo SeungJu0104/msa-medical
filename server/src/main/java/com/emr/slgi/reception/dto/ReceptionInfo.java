5bffdbe7f3729601dd71b088ec1f93194b147955
DESKTOP-684DG1A\SeungJu <skatmdwn23@naver.com>
2025-07-11 21:35:36 +0900

    fix: 예약에서 대기 상태로 전환 시  이름을 넣지 않는 문제 때문에 대기 목록을 가져올 때, TB_MEMBER 테이블과 조인해 이름 가져오도록 수정.

--- server/src/main/java/com/emr/slgi/reception/dto/ReceptionInfo.java
+++ server/src/main/java/com/emr/slgi/reception/dto/ReceptionInfo.java
@@ -20,14 +20,6 @@ public class ReceptionInfo {
     @Pattern(regexp = Validate.MEMBER_UUID_REGEX, message = ReservationErrorMessage.CAN_NOT_FIND_PATIENT)
     private String patientUuid;
 
-    @NotBlank
-    @Size(min = 2, max = 6)
-    private String patientName;
-
-    @NotBlank
-//    @Pattern(regexp = RegexPatterns.BIRTH_DATE, message = ReceptionErrorMessage.BIRTH_DATE_IS_NOT_VALID)
-    private String birthDate;
-
     @NotBlank(message = ReservationErrorMessage.WRITE_SYMPTOM)
     @Size(min = 1, max = 100)
     private String symptom;
