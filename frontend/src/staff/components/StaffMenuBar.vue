470777893f24e154a7ffcd5219dc0770bdd81f2e
DESKTOP-684DG1A\SeungJu <skatmdwn23@naver.com>
2025-07-12 00:51:00 +0900

    feat: StaffMenuBar.vue 생성 후 내용 작성 및 StaffMenuBar.css 작성.

---
+++ frontend/src/staff/components/StaffMenuBar.vue
@@ -0,0 +1,35 @@
+<script setup>
+import '@/css/staff/StaffMenuBar.css'
+import {useRouter} from "vue-router";
+
+  const router = useRouter();
+
+  const routerPush = (route) => {
+    router.push(route);
+  }
+
+
+
+</script>
+
+<template>
+  <div class="my-3">
+    <div class="menuContainer">
+      <div id="reservation" @click="routerPush({name: 'regReservation'})">
+        <span>예약 등록</span>
+      </div>
+      <div id="registration" @click="routerPush({name: 'regPatientByStaff'})">
+        <span>환자 등록</span>
+      </div>
+      <div id="searching" @click="routerPush({name: 'search'})">
+        <span>환자 검색</span>
+      </div>
+      <div id="payment" @click="routerPush({})">
+        <span>결제</span>
+      </div>
+      <div id="setting" @click="routerPush({})">
+        <span>설정</span>
+      </div>
+    </div>
+  </div>
+</template>
