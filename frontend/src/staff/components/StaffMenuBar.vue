c837d967a9560fc6754093afc847b25a0588667e
DESKTOP-684DG1A\SeungJu <skatmdwn23@naver.com>
2025-07-14 19:36:23 +0900

    design: 의료진 메뉴바 디자인 수정

--- frontend/src/staff/components/StaffMenuBar.vue
+++ frontend/src/staff/components/StaffMenuBar.vue
@@ -1,26 +1,38 @@
 <script setup>
-import '@/css/staff/StaffMenuBar.css'
+import '@/assets/css/StaffMenuBar.css'
 import LogoutButton from "@/auth/components/LogoutButton.vue";
+import {defineEmits} from 'vue'
+
+const emit = defineEmits(['open']);
+
+const menuItems = [
+  { label: '홈', icon: new URL('@/assets/icons/home.png', import.meta.url), to: { name: 'staffMain' } },
+  { label: '환자 검색', icon: new URL('@/assets/icons/search.png', import.meta.url), action: () => emit('open') },
+  { label: '환자 등록', icon: new URL('@/assets/icons/registration.png', import.meta.url), to: { name: 'regPatientByStaff' } },
+  { label: '예약 등록', icon: new URL('@/assets/icons/reservation.png', import.meta.url), to: { name: 'regReservation' } },
+  // { label: '통계', icon: new URL('@/assets/icons/stat.png', import.meta.url), to: { name: 'stat' } },
+  // { label: '설정', icon: new URL('@/assets/icons/settings.png', import.meta.url), to: { name: 'settings' } },
+]
+
 
 </script>
 
-<template>
-  <div class="my-3">
 
-    <div class="menuContainer">
-      <div class="my-3">
-        <router-link :to="{name: 'regPatientByStaff'}">환자 등록</router-link>
-      </div>
-      <div class="my-3">
-        <router-link :to="{name: 'search'}">환자 검색</router-link>
-      </div>
-      <div class="my-3">
-        <router-link :to="{name: ''}">결제</router-link>
-      </div>
-      <div class="my-3">
-        <router-link :to="{name: ''}">설정</router-link>
-      </div>
-      <LogoutButton/>
-    </div>
+<template>
+  <div class="sidebar">
+    <nav>
+      <ul>
+        <li v-for="item in menuItems" :key="item.label" class="menu-item">
+          <router-link v-if="item.to" :to="item.to">
+            <img :src="item.icon" alt="icon" class="icon" />
+            <span>{{ item.label }}</span>
+          </router-link>
+          <a v-else @click="item.action" class="menu-clickable">
+            <img :src="item.icon" alt="icon" class="icon" />
+            <span>{{ item.label }}</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
