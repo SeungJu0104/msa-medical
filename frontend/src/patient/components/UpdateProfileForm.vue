1e9b0ee1cf12191590cdfd08a6555e7537655e92
Conut-1 <1mim1@naver.com>
2025-07-14 16:07:58 +0900

    feat: 로딩 중에 스피너 출력

--- frontend/src/patient/components/UpdateProfileForm.vue
+++ frontend/src/patient/components/UpdateProfileForm.vue
@@ -1,27 +1,31 @@
 <template>
   <div class="card p-4 w-100" style="max-width: 720px;">
-    <form>
-      <div class="mb-3">
-        <label class="form-label">아이디</label>
-        <input type="text" :value="member.userid" class="form-control" disabled>
+    <Loading v-if="loading" />
+    <template v-else>
+      <form>
+        <div class="mb-3">
+          <label class="form-label">아이디</label>
+          <input type="text" :value="member.userid" class="form-control" disabled>
+        </div>
+        <div class="mb-3">
+          <label for="name" class="form-label">이름</label>
+          <input type="text" v-model="member.name" class="form-control" id="name">
+        </div>
+        <div class="mb-3">
+          <label for="phone" class="form-label">전화번호</label>
+          <input type="text" v-model="member.phone" class="form-control" id="phone">
+        </div>
+      </form>
+      <div class="d-flex justify-content-end gap-2">
+        <button @click="updateProfile" class="btn btn-primary">수정</button>
+        <button @click="goBack" class="btn btn-danger">취소</button>
       </div>
-      <div class="mb-3">
-        <label for="name" class="form-label">이름</label>
-        <input type="text" v-model="member.name" class="form-control" id="name">
-      </div>
-      <div class="mb-3">
-        <label for="phone" class="form-label">전화번호</label>
-        <input type="text" v-model="member.phone" class="form-control" id="phone">
-      </div>
-    </form>
-    <div class="d-flex justify-content-end gap-2">
-      <button @click="updateProfile" class="btn btn-primary">수정</button>
-      <button @click="goBack" class="btn btn-danger">취소</button>
-    </div>
+    </template>
   </div>
 </template>
 
 <script setup>
+import Loading from '@/common/components/Loading.vue';
 import { customFetch } from '@/util/customFetch';
 import { ENDPOINTS } from '@/util/endpoints';
 import { pick } from 'lodash';
@@ -30,11 +34,13 @@ import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
+const loading = ref(true);
 const member = ref({});
 
 onMounted(async () => {
   const response = await customFetch(ENDPOINTS.patient.profile);
   member.value = response.data.member;
+  loading.value = false;
 });
 
 async function updateProfile() {
