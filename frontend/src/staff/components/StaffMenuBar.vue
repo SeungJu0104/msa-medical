6347f34a5807f2a79833131fe60e18659d9e34db
DESKTOP-684DG1A\SeungJu <skatmdwn23@naver.com>
2025-07-15 21:54:39 +0900

    fix: StaffMenuBar 에 예약 등록 router-link 수정.

--- frontend/src/staff/components/StaffMenuBar.vue
+++ frontend/src/staff/components/StaffMenuBar.vue
@@ -9,14 +9,13 @@ const menuItems = [
   { label: '홈', icon: new URL('@/assets/icons/home.png', import.meta.url), to: { name: 'staffMain' } },
   { label: '환자 검색', icon: new URL('@/assets/icons/search.png', import.meta.url), action: () => emit('open') },
   { label: '환자 등록', icon: new URL('@/assets/icons/registration.png', import.meta.url), to: { name: 'regPatientByStaff' } },
-  { label: '예약 등록', icon: new URL('@/assets/icons/reservation.png', import.meta.url), to: { name: 'regReservation' } },
+  { label: '예약 등록', icon: new URL('@/assets/icons/reservation.png', import.meta.url), to: { name: 'regReservationByStaff' } },
   // { label: '통계', icon: new URL('@/assets/icons/stat.png', import.meta.url), to: { name: 'stat' } },
   // { label: '설정', icon: new URL('@/assets/icons/settings.png', import.meta.url), to: { name: 'settings' } },
 ]
 
 
 </script>
-
 
 <template>
   <div class="sidebar">
