6be9a0ad0af41df8c9a654b9019a139684733a0b
DESKTOP-684DG1A\SeungJu <skatmdwn23@naver.com>
2025-07-17 10:35:12 +0900

    fix: import 문 없어서 발생한 오류 수정.

--- frontend/src/reception/components/WaitingList.vue
+++ frontend/src/reception/components/WaitingList.vue
@@ -4,14 +4,10 @@ import {computed, onBeforeMount, onMounted, onUnmounted, ref} from "vue";
 import WaitingListDoctorName from "@/shared/components/WaitingListDoctorName.vue";
 import WaitingListPatientList from "@/shared/components/WaitingListPatientList.vue";
 import {reception} from "@/reception/util/reception.js";
-import { getStompClient, sendMsg, subscribeChannel } from "@/util/stompMethod.js";
-import { useUserStore } from "@/stores/userStore.js";
-import { getAccessToken } from "@/auth/accessToken.js";
-import { createWebSocketModuleRunnerTransport } from "vite/module-runner";
-import VisitHistory from "@/components/diagnosis/VisitHistory.vue";
-import {useRouter} from "vue-router";
+import { getStompClient, subscribeChannel } from "@/util/stompMethod.js";
 import "@/assets/css/ReservationListByStaff.css";
 import dayjs from "dayjs";
+
 
   const waitingListStore = useWaitingListStore();
   const waitingList = ref();
