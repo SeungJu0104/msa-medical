1715bef179a0dbd83f7eb3fb2e75050500b4a63c
Conut-1 <1mim1@naver.com>
2025-07-19 22:06:27 +0900

    refactor: 상수 사용

--- frontend/src/router/patientRoutes.js
+++ frontend/src/router/patientRoutes.js
@@ -1,4 +1,5 @@
 import { useUserStore } from '@/stores/userStore';
+import { roles } from '@/util/roles';
 
 const MainView = () => import('@/patient/views/MainView.vue');
 const MyPageView = () => import("@/patient/views/MyPageView.vue");
@@ -15,7 +16,7 @@ export const patientRoutes = [
     component: PatientLayout,
     beforeEnter: (to, from, next) => {
       const role = useUserStore().user?.role;
-      if (['NURSE', 'DOCTOR'].includes(role)) {
+      if ([roles.NURSE, roles.DOCTOR].includes(role)) {
         return next({ name: 'staffMain' });
       }
       next();
@@ -40,25 +41,25 @@ export const patientRoutes = [
         path: 'mypage',
         name: 'mypage',
         component: MyPageView,
-        meta: { requiresAuth: true,  roles: ['PATIENT'] },
+        meta: { requiresAuth: true,  roles: [roles.PATIENT] },
       },
       {
         path: 'profile/update',
         name: 'updateProfile',
         component: UpdateProfile,
-        meta: { requiresAuth: true,  roles: ['PATIENT'] },
+        meta: { requiresAuth: true,  roles: [roles.PATIENT] },
       },
       {
         path: 'regReservationByPatient',
         name: 'regReservationByPatient',
         component: RegReservationByPatient,
-        meta: { requiresAuth: true,  roles: ['PATIENT'] },
+        meta: { requiresAuth: true,  roles: [roles.PATIENT] },
       },
       {
         path: 'reservationListByPatient',
         name: 'reservationListByPatient',
         component: ReservationListByPatient,
-        meta: { requiresAuth: true,  roles: ['PATIENT'] },
+        meta: { requiresAuth: true,  roles: [roles.PATIENT] },
       },
     ]
   },
